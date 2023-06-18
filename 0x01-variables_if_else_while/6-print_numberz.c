6520578ffcd454c998baddbbb9194baaf82894b8
Nnamdiweb3 <macmon61@gmail.com>
2023-06-18 03:42:12 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * prints number from 0 - 9.
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
